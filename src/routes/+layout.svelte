ddbe88a67226479ef17a5e2badee9cc76e3e82bd
theTCA <t.mattfeld@gmx.de>
2024-03-20 00:10:10 +0100

    Added: header, navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,55 @@
 <script>
     import '../app.css';
+    import {page} from '$app/stores';
+
+    const links = [];
 </script>
 
-<slot></slot>
+<div class="drawer">
+    <input id="drawer" type="checkbox" class="drawer-toggle">
+    <div class="drawer-content flex flex-col">
+        <nav class="w-full navbar bg-base-300">
+            <div class="flex-none lg:hidden">
+                <label for="drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </label>
+            </div>
+            <div class="flex-none px-2 mx-2">
+                <a class="text-lg font-semibold" href="/">HFLÜ-Trainer</a>
+            </div>
+            <div class="flex-1 hidden lg:block">
+                <ul class="menu menu-horizontal gap-2">
+                    {#each links as link}
+                    <li>
+                        {#if $page.url.pathname === link.href}
+                        <a class="bg-base-100 active" href={link.href}>{link.name}</a>
+                        {:else}
+                        <a class="bg-base-100" href={link.href}>{link.name}</a>
+                        {/if}
+                    </li>
+                    {/each}
+                </ul>
+            </div>
+        </nav>
+        <main>
+            <slot/>
+        </main>
+    </div>
+    <div class="drawer-side">
+        <label for="drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 min-h-full gap-2 bg-base-200">
+            <li class="text-lg font-semibold">
+                HFLÜ-Trainer
+            </li>
+            {#each links as link}
+            <li>
+                {#if $page.url.pathname === link.href}
+                <a class="bg-base-100 active" href={link.href}>{link.name}</a>
+                {:else}
+                <a class="bg-base-100" href={link.href}>{link.name}</a>
+                {/if}
+            </li>
+            {/each}
+        </ul>
+    </div>
+</div>
