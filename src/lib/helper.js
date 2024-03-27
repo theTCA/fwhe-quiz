0636f60d1957b694e67e6dc807d77757120983f2
theTCA <t.mattfeld@gmx.de>
2024-03-27 04:07:06 +0100

    Added: helper file

---
+++ src/lib/helper.js
@@ -0,0 +1,40 @@
+/**
+ * Shuffle array
+ * @param {any[]} array the array to shuffle
+ * @returns {any[]} shuffled array
+ */
+export function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    return array;
+}
+
+
+/**
+ * Tesst if storage is supported and available
+ * @param {string} type
+ */
+export function storageAvailable(type) {
+    let storage;
+    try {
+      // @ts-ignore
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        (e.code === 22 ||
+         e.code === 1014 ||
+         e.name === "QuotaExceededError" ||
+         e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        storage && storage.length !== 0);
+    }
+  }
