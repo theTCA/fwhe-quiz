0ec3b9e5430b7cedc61eec45c01017abfa13d335
theTCA <t.mattfeld@gmx.de>
2024-04-01 14:46:15 +0200

    Fixed: history date not a date object

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -29,5 +29,5 @@ function putStore(key, value) {
 export const quizQuestions = writable(getStored("quizQuestions", []).map((/** @type {number} */ e) => flattendQuestions.find(q => q.id === e)));
 quizQuestions.subscribe(val => putStore("quizQuestions", val.map((/** @type {{ id: number; }} */ q) => q.id)));
 
-export const quizHistory = writable(getStored("quizHistory", []).map((/**@type {{id: number, answer: string, date:Date}} */ e) => ({question: flattendQuestions.find(q => q.id === e.id), answer: e.answer, date: e.date})));
+export const quizHistory = writable(getStored("quizHistory", []).map((/**@type {{id: number, answer: string, date:Date}} */ e) => ({question: flattendQuestions.find(q => q.id === e.id), answer: e.answer, date: new Date(e.date)})));
 quizHistory.subscribe(val => putStore("quizHistory", val.map(( /** @type {{ question: import("$lib/types").Question; answer: any; date: any; }} */ h) => ({id: h.question.id, answer: h.answer, date: h.date}))));
