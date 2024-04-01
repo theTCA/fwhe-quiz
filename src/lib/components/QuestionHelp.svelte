b69fe8110528555b5e9d1a016504fba2e117db17
theTCA <t.mattfeld@gmx.de>
2024-04-01 22:36:16 +0200

    Fixed: mobile layout

--- src/lib/components/QuestionHelp.svelte
+++ src/lib/components/QuestionHelp.svelte
@@ -8,12 +8,17 @@
 </script>
 
 {#if questionHelp}
-<div class="flex flex-wrap">
+<div class="flex flex-col gap-2">
     {#each questionHelp as help}
-    <a class="btn flex-col flex-1" href={`${help.source}#page=${help.page}`} target="_blank">
-        <h4 class="text-sm font-bold"> {help.name} </h4>
+    <a class="btn flex flex-col flex-nowrap justify-center items-center text-sm h-full gap-0.5" href={`${help.source}#page=${help.page}`} target="_blank">
+        <h4 class="font-bold"> {help.name} </h4>
         <div>
-            {help.description}
+            <div>
+                {help.description}
+            </div>
+            <div class="text-xs mt-0.5">
+                Seite {help.page}
+            </div>
         </div>
     </a>
     {/each}
