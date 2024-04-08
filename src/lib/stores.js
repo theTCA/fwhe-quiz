ffbe77749401f97b66cdd9e9527c08ab99b0cca6
theTCA <t.mattfeld@gmx.de>
2024-04-08 13:02:45 +0200

    Renamed: function names

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -6,7 +6,7 @@ import { flattendQuestions } from "./data";
  * @param {string} key
  * @param {*} base
  */
-function getStored(key, base) {
+function getLocalStorage(key, base) {
     if(!storageAvailable("localStorage"))
         return base;
     let item = localStorage.getItem(key);
@@ -20,26 +20,26 @@ function getStored(key, base) {
  * @param {string} key
  * @param {*} value
  */
-function putStore(key, value) {
+function putLocalStorage(key, value) {
     if(!storageAvailable("localStorage"))
         return;
     localStorage.setItem(key, JSON.stringify(value));
 }
 
-export const quizQuestions = writable(getStored("quizQuestions", []).map((/** @type {number} */ e) => flattendQuestions.find(q => q.id === e)));
-quizQuestions.subscribe(val => putStore("quizQuestions", val.map((/** @type {{ id: number; }} */ q) => q.id)));
+export const quizQuestions = writable(getLocalStorage("quizQuestions", []).map((/** @type {number} */ e) => flattendQuestions.find(q => q.id === e)));
+quizQuestions.subscribe(val => putLocalStorage("quizQuestions", val.map((/** @type {{ id: number; }} */ q) => q.id)));
 
-export const quizHistory = writable(getStored("quizHistory", []).map((/**@type {{id: number, answer: string, date:Date}} */ e) => ({question: flattendQuestions.find(q => q.id === e.id), answer: e.answer, date: new Date(e.date)})));
-quizHistory.subscribe(val => putStore("quizHistory", val.map(( /** @type {import("$lib/types").QuizHistory} */ h) => ({id: h.question.id, answer: h.answer, date: h.date}))));
+export const quizHistory = writable(getLocalStorage("quizHistory", []).map((/**@type {{id: number, answer: string, date:Date}} */ e) => ({question: flattendQuestions.find(q => q.id === e.id), answer: e.answer, date: new Date(e.date)})));
+quizHistory.subscribe(val => putLocalStorage("quizHistory", val.map(( /** @type {import("$lib/types").QuizHistory} */ h) => ({id: h.question.id, answer: h.answer, date: h.date}))));
 
-export const trainingChapters = writable(getStored("trainingChapters", []));
-trainingChapters.subscribe(val => putStore("trainingChapters", val));
+export const trainingChapters = writable(getLocalStorage("trainingChapters", []));
+trainingChapters.subscribe(val => putLocalStorage("trainingChapters", val));
 
-export const trainingLastChapter = writable(getStored("trainingLastChapter", ""));
-trainingLastChapter.subscribe(val => putStore("trainingLastChapter", val));
+export const trainingLastChapter = writable(getLocalStorage("trainingLastChapter", ""));
+trainingLastChapter.subscribe(val => putLocalStorage("trainingLastChapter", val));
 
-export const bookmarks = writable(getStored("bookmarks", []).map((/** @type {number} */ m) => flattendQuestions.find(q => q.id === m)));
-bookmarks.subscribe(val => putStore("bookmarks", val.map((/** @type { import("$lib/types").Question } */ q) => q.id)));
+export const bookmarks = writable(getLocalStorage("bookmarks", []).map((/** @type {number} */ m) => flattendQuestions.find(q => q.id === m)));
+bookmarks.subscribe(val => putLocalStorage("bookmarks", val.map((/** @type { import("$lib/types").Question } */ q) => q.id)));
 
-export const theme = writable(getStored("theme", ""));
-theme.subscribe(val => putStore("theme", val));
+export const theme = writable(getLocalStorage("theme", ""));
+theme.subscribe(val => putLocalStorage("theme", val));
