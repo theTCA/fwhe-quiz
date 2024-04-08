2c48f8562919f7d2a966b8549ab6214031ebf290
theTCA <t.mattfeld@gmx.de>
2024-04-08 12:52:11 +0200

    Added: selectable deletion range

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -4,6 +4,7 @@
     import {quizHistory} from "$lib/stores";
 
     $: historyRange = 10;
+    $: deleteRange = 10;
     $: correctQuestions = $quizHistory.reduce(reduceCorrectQuestions, 0);
     $: correctQuestionsProgress = $quizHistory.length > 0 ? Math.round((correctQuestions / $quizHistory.length) * 100) : 0;
     $: historyAsCatalogues = $quizHistory.reduce(reduceCatalogues, {});
@@ -41,6 +42,14 @@
         }
         return acc;
     }
+
+    function deleteHistory() {
+        if($quizHistory.length <= deleteRange) {
+            $quizHistory = [];
+        } else {
+            $quizHistory = $quizHistory.slice(0, $quizHistory.length - deleteRange);
+        }
+    }
 </script>
 
 <svelte:head>
@@ -73,8 +82,9 @@
         </div>
     </div>
     <div class="divider"></div>
-    <div class="flex justify-between mb-2">
-        <div>
+    <div class="flex flex-col gap-4">
+        {#if $quizHistory.length > 0}
+        <div class="flex flex-row items-center gap-2">
             <span class="font-bold"> Letzten </span>
             <select class="select select-bordered select-sm" bind:value={historyRange}>
                 <option value={10}>10</option>
@@ -84,11 +94,25 @@
             </select>
             <span class="font-bold"> Fragen</span>
         </div>
-        <button class="btn btn-error btn-sm" on:click={() => $quizHistory = []}>Löschen</button>
-    </div>
-    <div class="flex flex-col gap-1">
-        {#each $quizHistory.slice(0, historyRange) as history}
-        <QuizHistory {history} />
-        {/each}
+        {/if}
+        <div class="flex flex-col gap-1">
+            {#each $quizHistory.slice(0, historyRange) as history}
+            <QuizHistory {history} />
+            {/each}
+        </div>
+        {#if $quizHistory.length > 0}
+        <div class="flex items-center gap-2">
+            <span class="font-bold">ältesten</span>
+            <div class="join">
+                <select class="join-item select select-error select-sm" bind:value={deleteRange}>
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                    <option value={50}>50</option>
+                    <option value={$quizHistory.length}>Alle</option>
+                </select>
+                <button class="join-item btn btn-error btn-sm" on:click={deleteHistory}>Löschen</button>
+            </div>
+        </div>
+        {/if}
     </div>
 </div>
