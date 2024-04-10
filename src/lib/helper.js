373a659f9ebb245480c1a489e13019e515f63316
theTCA <t.mattfeld@gmx.de>
2024-04-10 15:14:36 +0200

    Formatting

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -22,19 +22,19 @@ export function shuffle(array) {
 export function storageAvailable(type) {
     let storage;
     try {
-      // @ts-ignore
-      storage = window[type];
-      const x = "__storage_test__";
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
+        // @ts-ignore
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
     } catch (e) {
-      return (
-        e instanceof DOMException &&
-        (e.code === 22 ||
-         e.code === 1014 ||
-         e.name === "QuotaExceededError" ||
-         e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-        storage && storage.length !== 0);
+        return (
+            e instanceof DOMException &&
+            (e.code === 22 ||
+                e.code === 1014 ||
+                e.name === "QuotaExceededError" ||
+                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            storage && storage.length !== 0);
     }
-  }
+}
