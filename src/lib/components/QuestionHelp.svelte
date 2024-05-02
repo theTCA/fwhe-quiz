5f4dd91e73f0349e6cd5ef7d55ff827931b99acc
theTCA <t.mattfeld@gmx.de>
2024-05-02 11:04:25 +0200

    Fixed: safari browsers not opening pdf on correct page

--- src/lib/components/QuestionHelp.svelte
+++ src/lib/components/QuestionHelp.svelte
@@ -1,16 +1,23 @@
 <script>
+    import {UAParser} from 'ua-parser-js';
     import {helps} from "$lib/data.json";
     /**
      * @type {import("$lib/types").Question}
      */
     export let question;
     const questionHelp = question.help.map(q => ({...helps.find((/** @type {{ name: string; }} */ h) => h.name === q.name), page: q.page}));
+
+    function isSafari() {
+        const uaParserResult = UAParser();
+        return uaParserResult.browser.name && uaParserResult.browser.name .includes("Safari");
+    }
 </script>
 
 {#if questionHelp}
 <div class="flex flex-col gap-2">
     {#each questionHelp as help}
-    <a class="btn flex flex-col flex-nowrap justify-center items-center text-sm h-full gap-0.5" href={`${help.source}#page=${help.page}`} target="_blank">
+    {@const url = `${help.source}#page${isSafari() ? "" : "="}${help.page}`}
+    <a class="btn flex flex-col flex-nowrap justify-center items-center text-sm h-full gap-0.5" href={url} target="_blank">
         <h4 class="font-bold"> {help.name} </h4>
         <div>
             <div>
