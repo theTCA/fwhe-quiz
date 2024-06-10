c64f3839990db4792f9defd63f751d10efaeeaf7
theTCA <t.mattfeld@gmx.de>
2024-06-10 15:30:21 +0200

    Added: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { page } from "$app/stores";
+    import { PUBLIC_APP_NAME } from "$env/static/public";
+</script>
+
+<svelte:head>
+    <title>Error {$page.status} - {PUBLIC_APP_NAME}</title>
+</svelte:head>
+
+<div class="w-full h-full flex flex-col items-center justify-center gap-2">
+    <div class="w-60 h-60 border-8 border-base-300 rounded-full flex justify-center items-center">
+        <h2 class="font-bold text-primary text-6xl"> {$page.status} </h2>
+    </div>
+    <div class="font-semibold text-lg">
+        {$page.error?.message}
+    </div>
+</div>
