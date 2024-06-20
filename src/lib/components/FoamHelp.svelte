f9bb78ad2abf5e69791ed37af8ad6d99c1c7d9f1
theTCA <t.mattfeld@gmx.de>
2024-06-20 16:10:44 +0200

    Changed: MathML into mathlifier

--- src/lib/components/FoamHelp.svelte
+++ src/lib/components/FoamHelp.svelte
@@ -1,116 +1,32 @@
 <script>
+    import { base } from "$app/paths";
+    import { alignStar } from "mathlifier";
+
     export let question;
 </script>
 
-<div class="flex flex-col items-center justify-center gap-3 mb-5">
-    <h3 class="font-semibold text-lg text-center"> Berechnung </h3>
+<svelte:head>
+    <link rel="stylesheet" href={base + "/katex.min.css"}>
+</svelte:head>
+
+<div>
     {#if question.type.id === "foamVolume"}
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mtext> Schaumvolumen </mtext>
-            <mo> = </mo>
-            <mtext> Durchflussrate </mtext>
-            <mo> &times; </mo>
-            <mtext> Verschäumungszahl </mtext>
-            <mo> &times; </mo>
-            <mtext> Zeit </mtext>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mn> {question.nozzle.flowRate} </mn>
-            <mfrac>
-                <mrow>
-                    <mtext> l </mtext>
-                </mrow>
-                <mrow>
-                    <mtext> min </mtext>
-                </mrow>
-            </mfrac>
-            <mo> &times; </mo>
-            <mn> {question.nozzle.foamRatio} </mn>
-            <mo> &times; </mo>
-            <mn> {question.time} </mn>
-            <mtext> min </mtext>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mn> {question.answer} </mn>
-            <mtext> l </mtext>
-        </math>
+    {@html alignStar(`
+        \\text{Schaumvolumen} &= \\text{Durchflussrate} \\times \\text{Verschäumungszahl} \\times \\text{Zeit} \\\\
+                              &= ${question.nozzle.flowRate} \\frac{\\text{l}}{\\text{min}} \\times ${question.nozzle.foamRatio} \\times ${question.time} \\text{min} \\\\
+                              &= ${question.answer} \\text{l}
+    `)}
     {:else if question.type.id === "agentVolume"}
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mtext> Schaummittelmenge </mtext>
-            <mo> = </mo>
-            <mtext> Durchflussrate </mtext>
-            <mo> &times; </mo>
-            <mtext> Zumischrate</mtext>
-            <mo> &times; </mo>
-            <mtext> Zeit </mtext>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mn> {question.mixer.flowRate} </mn>
-            <mfrac>
-                <mrow>
-                    <mtext> l </mtext>
-                </mrow>
-                <mrow>
-                    <mtext> min </mtext>
-                </mrow>
-            </mfrac>
-            <mo> &times; </mo>
-            <mn> {question.mixer.mixingRate} </mn>
-            <mo> % </mo>
-            <mo> &times; </mo>
-            <mn> {question.time} </mn>
-            <mtext> min </mtext>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mn> {question.answer} </mn>
-            <mtext> l </mtext>
-        </math>
+    {@html alignStar(`
+        \\text{Schaummittelmenge} &= \\text{Durchflussrate} \\times \\text{Zumischrate} \\times \\text{Zeit} \\\\
+                                  &= ${question.mixer.flowRate} \\frac{\\text{l}}{\\text{min}} \\times ${question.mixer.mixingRate} \\times ${question.time} \\\\
+                                  &= ${question.answer} \\text{l}
+    `)}
     {:else if question.type.id === "time"}
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mtext> Zeit </mtext>
-            <mo> = </mo>
-            <mfrac>
-                <mrow>
-                    <mtext> Schaummittelmenge </mtext>
-                </mrow>
-                <mrow>
-                    <mtext> Durchflussrate </mtext>
-                    <mo> &times; </mo>
-                    <mtext> Zumischrate </mtext>
-                </mrow>
-            </mfrac>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mfrac>
-                <mrow>
-                    <mn> {question.agentAmount} </mn>
-                    <mtext> l </mtext>
-                </mrow>
-                <mrow>
-                    <mn> {question.mixer.flowRate} </mn>
-                    <mfrac>
-                        <mrow>
-                            <mtext> l </mtext>
-                        </mrow>
-                        <mrow>
-                            <mtext> min </mtext>
-                        </mrow>
-                    </mfrac>
-                    <mo> &times; </mo>
-                    <mn> {question.mixer.mixingRate} </mn>
-                    <mtext> % </mtext>
-                </mrow>
-            </mfrac>
-        </math>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mo> = </mo>
-            <mn> {question.answer} </mn>
-            <mtext> min </mtext>
-        </math>
+    {@html alignStar(`
+        \\text{Zeit} &= \\frac{\\text{Schaummittelmenge}}{\\text{Durchflussrate} \\times \\text{Zumischrate}} \\\\
+                     &= \\frac{${question.agentAmount}}{${question.mixer.flowRate} \\frac{\\text{l}}{\\text{min}}  \\times ${question.mixer.mixingRate}\\%} \\\\
+                     &= ${question.answer} \\text{min}
+    `)}
     {/if}
 </div>
