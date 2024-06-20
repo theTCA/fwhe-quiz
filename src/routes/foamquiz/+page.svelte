2b3c51ac909b7029ce52cbb882c2dbdeb6500977
theTCA <t.mattfeld@gmx.de>
2024-06-20 19:04:51 +0200

    Changed: styling

--- src/routes/foamquiz/+page.svelte
+++ src/routes/foamquiz/+page.svelte
@@ -3,6 +3,7 @@
     import { shuffle } from "$lib/helper";
     import FoamHelp from "$lib/components/FoamHelp.svelte";
     import FoamQuestion from "$lib/components/FoamQuestion.svelte";
+    import Icon from "@iconify/svelte";
 
     const mixers = [
         {
@@ -74,13 +75,7 @@
         }
     ];
 
-    /**
-     * @type {string[]}
-     */
-    let selectedTypes = [];
-    let selectedAll = false;
-    let compactView = true;
-
+    let compactView = false;
     let questions = generateQuestions();
     $: question = questions[0];
 
@@ -116,7 +111,7 @@
                             if(type.id === "foamVolume") {
                                 question.mixer.mixingRate = shuffle(mixingRates)[0];
                             }
-                            question.answer = Number(question.type.calcFunc(question).toPrecision(2)).toString().replace(".", ",");
+                            question.answer = Number(question.type.calcFunc(question).toPrecision(2)).toString();
                             questions = [...questions, question];
                             if(type.id === "foamVolume") {
                                 break;
@@ -126,7 +121,6 @@
                 }
             }
         }
-        console.log(questions);
         return shuffle(questions);
     }
 
@@ -170,53 +164,48 @@
     <div class="relative flex justify-between w-full mb-2">
         <h1 class="text-3xl flex-1 text-center font-semibold mb-2"> Schaumquiz </h1>
     </div>
-    <!--
-        <div class="flex flex-col gap-2">
-            <h2 class="text-lg font-semibold text-center"> Fragetypen </h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-                {#each quizTypes as type}
-                <label class="flex items-center gap-2 p-2 bg-base-200 rounded-md cursor-pointer select-none">
-                    <input class="checkbox checkbox-primary rounded-full bg-base-100" type="checkbox" value={type.id} checked={selectedTypes.includes(type.id)} bind:group={selectedTypes}>
-                    {type.name}
-                </label>
-                {/each}
-            </div>
-            <div class="flex justify-center">
-                <button class="btn btn-primary" on:click={() => {}}> Wählen </button>
-            </div>
-        </div>
-        -->
     <div class="flex flex-col justify-center gap-2">
-        <h2 class="text-2xl flex-1 text-center font-semibold mb-2"> {question.type.name} </h2>
-        <input type="checkbox" bind:checked={compactView}>
+        <div class="relative">
+            <h2 class="text-2xl flex-1 text-center font-semibold mb-2"> {question.type.name} </h2>
+            <label class="swap btn absolute -top-2 right-1">
+                <input type="checkbox" bind:checked={compactView}>
+                <Icon class="swap-on" icon="solar:text-bold"/>
+                <Icon class="swap-off" icon="octicon:number-16"/>
+            </label>
+        </div>
         <FoamQuestion {question} {compactView}/>
-        <form class="flex flex-col gap-2">
+        <form class="flex items-center flex-col gap-2 border-t-2 border-t-base-300 p-2">
             {#if !answered}
-            <div class="flex-1 grid grid-cols-1 lg:grid-cols-2 gap-2 items-center">
-                <input class="input input-bordered text-center lg:text-right pr-1" type="text" bind:value={answer}>
-                <span class="font-semibold text-center lg:text-left"> {question.type.answerUnit} </span>
-            </div>
-            <div class="flex justify-center">
-                <button class="btn btn-primary btn-outline w-full md:w-1/2" type="submit" on:click|preventDefault={answerQuestion}> Wählen </button>
+            <div class="flex-1 flex flex-col gap-2 w-full lg:w-1/2">
+                <input class="input input-bordered text-center text-lg w-full lg:w-1/2 inline-flex self-center" type="text" bind:value={answer}>
+                <span class="font-semibold text-center"> {question.type.answerUnit} </span>
             </div>
             {:else}
-            <div class="py-3">
                 {#if question.answer === answer}
-                <div class=" grid grid-cols-2 gap-2 text-center text-success">
-                    <span class="text-right pr-1"> {answer} </span>
-                    <span class="font-semibold text-left"> {question.type.answerUnit} </span>
+                <div class="flex flex-col gap-2 w-full text-center text-success font-semibold">
+                    <div class="flex justify-center items-center min-h-12">
+                        <span class="text-lg"> {answer} </span>
+                    </div>
+                    <span> {question.type.answerUnit} </span>
                 </div>
                 {:else}
-                <div class=" grid grid-cols-2 gap-2 text-center text-error">
-                    <span class="text-right pr-1"> {answer} </span>
-                    <span class="font-semibold text-left"> {question.type.answerUnit} </span>
+                <div class="flex flex-col gap-2 w-full text-center text-error font-semibold">
+                    <div class="flex justify-center items-center min-h-12">
+                        <span class="text-lg"> {answer} </span>
+                    </div>
+                    <span> {question.type.answerUnit} </span>
                 </div>
                 {/if}
-            </div>
-            <div class="flex justify-center">
-                <button class="btn btn-primary w-full md:w-1/2" type="submit" on:click|preventDefault={nextQuestion}> Weiter </button>
-            </div>
             {/if}
+            <div class="flex justify-center w-full lg:w-1/2 mt-3">
+                <button class="btn btn-primary btn-outline w-full" type="submit" on:click|preventDefault={() => answered ? nextQuestion() : answerQuestion()}>
+                    {#if !answered}
+                    Wählen
+                    {:else}
+                    Weiter
+                    {/if}
+                </button>
+            </div>
         </form>
         {#if answered}
         <FoamHelp {question}/>
