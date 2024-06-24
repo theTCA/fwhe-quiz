51e49f125bccf0b11fb844d07607530673fe282c
theTCA <t.mattfeld@gmx.de>
2024-06-24 15:27:27 +0200

    Added: search filter

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,3 +1,4 @@
 import {catalogues} from "./data.json";
 
 export const flattendQuestions = catalogues.map(c => c.questions.map(q => ({...q, catalog: c.name}))).flat();
+export const allQuestionTags = flattendQuestions.reduce((/** @type {string[]} */ acc, q) => Array.from(new Set([...acc, ...q.tags])), []).sort((a, b) => a.localeCompare(b));
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -5,8 +5,14 @@
     import QuestionHelp from "$lib/components/QuestionHelp.svelte";
     import { PUBLIC_APP_NAME } from "$env/static/public";
     import { onMount } from "svelte";
+    import { allQuestionTags, flattendQuestions } from "$lib/data";
 
     let searchInput = "";
+    let selectedTag = allQuestionTags[0];
+
+    /** @type {string[]} */
+    let selectedTags = [];
+
     $: questions = getCatalogues();
 
     $: openedQuestion = 0;
@@ -26,30 +32,74 @@
     ];
 
     function search() {
-        if(searchInput.length > 0) {
-            questions = getCatalogues().map(c => ({...c, questions: c.questions.map(q => ({...q, hidden: !filterQuestion(q)}))})).filter(c => c.questions.reduce((acc, q) => q.hidden ? acc : acc + 1, 0) > 0);
-        } else {
-            questions = getCatalogues();
-        }
+        let hiddenQuestions = [...filterQuestionSearch(), ...filterQuestionTags()];
+        questions = getCatalogues().map(c => ({...c, questions: c.questions.map(q => ({...q, hidden: hiddenQuestions.includes(q.id)}))})).filter(c => c.questions.reduce((acc, q) => q.hidden ? acc : acc + 1, 0) > 0);
         questions = questions;
     }
 
-    function clear() {
+    function clearSearch() {
         searchInput = "";
         search();
     }
 
-    /**
-     * @param {import("$lib/types").Question} question
-     */
-    function filterQuestion(question) {
-        if(searchToggles.at(0)?.value && question.question.toLowerCase().includes(searchInput.toLowerCase())) return true;
-        if(searchToggles.at(1)?.value && question.choices.some(c => c.text.toLowerCase().includes(searchInput.toLowerCase()))) return true;
-        return false;
+    function clearTags() {
+        selectedTags = [];
+        search();
+    }
+
+    function filterQuestionSearch() {
+        /**
+         * @type {number[]}
+         */
+        let hiddenQuestions = [];
+        if(searchInput.length > 0) {
+            for(let question of flattendQuestions) {
+                if(searchToggles.at(0)?.value && question.question.toLowerCase().includes(searchInput.toLowerCase())) {
+                    continue;
+                }
+                if(searchToggles.at(1)?.value && question.choices.some(c => c.text.toLowerCase().includes(searchInput.toLowerCase()))) {
+                    continue;
+                }
+                hiddenQuestions = [...hiddenQuestions, question.id];
+            }
+        }
+        return hiddenQuestions;
+    }
+
+    function filterQuestionTags() {
+        /**
+         * @type {number[]}
+         */
+        let hiddenQuestions = [];
+        if(selectedTags.length > 0) {
+            for(let question of flattendQuestions) {
+                for(let tag of selectedTags) {
+                    if(!question.tags.includes(tag)) {
+                        hiddenQuestions = [...hiddenQuestions, question.id];
+                        break;
+                    }
+                }
+            }
+        }
+        return hiddenQuestions;
     }
 
     function getCatalogues() {
         return catalogues.map(c => ({name: c.name, questions: c.questions.map(q => ({...q, hidden: false}))}));
+    }
+
+    function selectTag() {
+        if(selectedTag && selectedTag.length > 0)
+            selectedTags = Array.from(new Set([...selectedTags, selectedTag])).sort((a, b) => a.localeCompare(b));
+        search();
+    }
+
+    /**
+     * @param {string} tag
+     */
+    function removeTag(tag) {
+        selectedTags = selectedTags.filter(t => t !== tag);
+        search();
     }
 </script>
 
@@ -63,7 +113,7 @@
         <div class="join flex">
             <div class="flex-1 relative">
                 <input class="join-item input input-bordered w-full" type="text" placeholder="Suche" bind:value={searchInput}>
-                <button class="absolute right-4 top-4 text-error" type="button" on:click={clear}>
+                <button class="absolute right-4 top-4 text-error" type="button" on:click={clearSearch}>
                     <Icon icon="material-symbols:close"/>
                 </button>
             </div>
@@ -72,14 +122,50 @@
             </button>
         </div>
         <div class="px-4">
-            {#each searchToggles as toggle}
-            <div class="form-control">
-                <label class="label">
-                    <span class="label-text">{toggle.name}</span>
-                    <input class="toggle toggle-primary" type="checkbox" bind:checked={toggle.value}>
-                </label>
+            <div>
+                {#each searchToggles as toggle}
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">{toggle.name}</span>
+                        <input class="toggle toggle-primary" type="checkbox" bind:checked={toggle.value}>
+                    </label>
+                </div>
+                {/each}
             </div>
-            {/each}
+            <div class="flex flex-col gap-2">
+                <h2 class="font-semibold"> Schlagwörter </h2>
+                <div class="grid grid-cols-1 lg:grid-cols-3 gap-2">
+                    <div class="flex flex-col gap-2">
+                        <div class="join">
+                            <select class="join-item select select-bordered" bind:value={selectedTag}>
+                                {#each allQuestionTags as tag}
+                                    <option value={tag}>{tag}</option>
+                                {/each}
+                            </select>
+                            <button class="join-item btn btn-primary btn-square" type="button" on:click={selectTag}>
+                                <Icon icon="mingcute:add-fill"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-span-2 flex flex-row flex-wrap gap-2">
+                        {#each selectedTags as tag}
+                        <div>
+                            <div class="join">
+                                <span class="join-item text-sm px-1 border font-semibold"> {tag} </span>
+                                <button class="join-item btn btn-xs text-error" type="button" on:click={() => removeTag(tag)}>
+                                    <Icon icon="carbon:close-filled"/>
+                                </button>
+                            </div>
+                        </div>
+                        {/each}
+                    </div>
+                </div>
+                <div>
+                    <button class="btn btn-error btn-outline btn-sm btn-block" type="button" on:click={clearTags}>
+                        Zurücksetzten
+                    </button>
+                </div>
+            </div>
         </div>
     </form>
     <div class="divider"></div>
@@ -112,9 +198,7 @@
                                 </div>
                                 {/each}
                             </div>
-                            {#if question.help.length > 0}
                             <QuestionHelp {question}/>
-                            {/if}
                         </div>
                     </Collapsible>
                     {/each}
