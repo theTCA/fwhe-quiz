50848b1a9c74915e5f7aef34de4e2b9812038505
theTCA <t.mattfeld@gmx.de>
2026-01-11 21:59:32 +0100

    Changed: svelte5 compatiblity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,120 +1,122 @@
 <script>
-    import "../app.css";
-    import { onMount } from "svelte";
-    import { base } from "$app/paths";
-    import { page } from "$app/state";
-    import { meta } from "$lib/data.json";
-    import { pages, pageGroups } from "$lib/pages";
-    import { version, clearVersionDependentStores } from "$lib/stores";
-    import { PUBLIC_APP_NAME } from "$env/static/public";
-    import Icon from "@iconify/svelte";
-    import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { asset, resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import { meta } from '$lib/data.json';
+	import { pages, pageGroups } from '$lib/pages';
+	import { version, clearVersionDependentStores } from '$lib/stores';
+	import { PUBLIC_APP_NAME } from '$env/static/public';
+	import Icon from '@iconify/svelte';
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
 
-    onMount(() => {
-        versionCheck();
-    });
+	let { children } = $props();
 
-    /**
-     * @type {HTMLInputElement}
-     */
-    let drawer;
+	onMount(() => {
+		versionCheck();
+	});
 
-    function closeDrawer() {
-        drawer.checked = false;
-    }
+	/**
+	 * @type {HTMLInputElement}
+	 */
+	let drawer;
 
-    function versionCheck() {
-        if($version < meta.version) {
-            clearVersionDependentStores();
-            $version = meta.version;
-        }
-    }
+	function closeDrawer() {
+		drawer.checked = false;
+	}
+
+	function versionCheck() {
+		if ($version < meta.version) {
+			clearVersionDependentStores();
+			$version = meta.version;
+		}
+	}
 </script>
 
 <svelte:head>
-    <link rel="apple-touch-icon" sizes="180x180" href={ base + "/apple-touch-icon.png"}>
-    <link rel="icon" type="image/png" sizes="32x32" href={base + "/favicon-32x32.png"}>
-    <link rel="icon" type="image/png" sizes="16x16" href={base + "/favicon-16x16.png"}>
-    <link rel="manifest" href={base + "/site.webmanifest"}>
-    <link rel="mask-icon" href={base + "/safari-pinned-tab.svg"} color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+	<link rel="apple-touch-icon" sizes="180x180" href={asset('/apple-touch-icon.png')} />
+	<link rel="icon" type="image/png" sizes="32x32" href={asset('/favicon-32x32.png')} />
+	<link rel="icon" type="image/png" sizes="16x16" href={asset('/favicon-16x16.png')} />
+	<link rel="manifest" href={asset('/site.webmanifest')} />
+	<link rel="mask-icon" href={asset('/safari-pinned-tab.svg')} color="#5bbad5" />
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <div class="drawer">
-    <input id="drawer" type="checkbox" class="drawer-toggle" bind:this={drawer}>
-    <div class="drawer-content flex flex-col">
-        <nav class="w-full navbar bg-base-300">
-            <div class="flex-none lg:hidden">
-                <label for="drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
-                    <Icon class="w-7 h-7" icon="ci:hamburger-md"/>
-                </label>
-            </div>
-            <div class="flex-none mr-2">
-                <a class="btn btn-ghost text-lg font-semibold" href={base + "/"}>
-                    <Icon icon="openmoji:fire-engine"/>
-                    {PUBLIC_APP_NAME}
-                </a>
-            </div>
-            <div class="flex-1 hidden lg:block">
-                <ul class="menu menu-horizontal gap-2">
-                    {#each pages.filter(p => p.inNavbar) as link}
-                    <li class="flex items-center">
-                        {#if page.url.pathname === link.href}
-                        <a class="btn btn-sm btn-active" href={link.href}>
-                            <Icon icon={link.icon}/>
-                            {link.name}
-                        </a>
-                        {:else}
-                        <a class="btn btn-sm" href={link.href}>
-                            <Icon icon={link.icon}/>
-                            {link.name}
-                        </a>
-                        {/if}
-                    </li>
-                    {/each}
-                </ul>
-            </div>
-            <div class="flex-1 inline-flex lg:flex-none gap-3 items-center justify-end">
-                    <ThemeSwitcher/>
-                    <a href={base + "/bookmarks"} class="btn btn-square">
-                        <Icon class="h-5 w-5" icon="material-symbols:bookmark"/>
-                    </a>
-            </div>
-        </nav>
-        <main class="w-full lg:w-[80%] mx-auto p-1 mt-2">
-            <slot/>
-        </main>
-    </div>
-    <div class="drawer-side">
-        <label for="drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu p-4 w-80 min-h-full gap-2 bg-base-200">
-            <li class="text-lg font-semibold">
-                <a href={`${base}/`} on:click={closeDrawer}>
-                    <Icon icon="openmoji:fire-engine"/>
-                    {PUBLIC_APP_NAME}
-                </a>
-            </li>
-            {#each pageGroups as group}
-            <li class="text-primary">
-                {group.name}
-            </li>
-            {#each group.pages as link}
-            <li>
-                {#if page.url.pathname === link.href}
-                <a class="bg-base-100 active" href={link.href} on:click={closeDrawer}>
-                    <Icon icon={link.icon}/>
-                    {link.name}
-                </a>
-                {:else}
-                <a class="bg-base-100" href={link.href} on:click={closeDrawer}>
-                    <Icon icon={link.icon}/>
-                    {link.name}
-                </a>
-                {/if}
-            </li>
-            {/each}
-            {/each}
-        </ul>
-    </div>
+	<input id="drawer" type="checkbox" class="drawer-toggle" bind:this={drawer} />
+	<div class="drawer-content flex flex-col">
+		<nav class="navbar w-full bg-base-300">
+			<div class="flex-none lg:hidden">
+				<label for="drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<Icon class="h-7 w-7" icon="ci:hamburger-md" />
+				</label>
+			</div>
+			<div class="mr-2 flex-none">
+				<a class="btn text-lg font-semibold btn-ghost" href={resolve('/')}>
+					<Icon icon="openmoji:fire-engine" />
+					{PUBLIC_APP_NAME}
+				</a>
+			</div>
+			<div class="hidden flex-1 lg:block">
+				<ul class="menu menu-horizontal gap-2">
+					{#each pages.filter((p) => p.inNavbar) as link ('menu' + link.name)}
+						<li class="flex items-center">
+							{#if page.url.pathname === link.href}
+								<a class="btn btn-active btn-sm" href={resolve(link.href)}>
+									<Icon icon={link.icon} />
+									{link.name}
+								</a>
+							{:else}
+								<a class="btn btn-sm" href={resolve(link.href)}>
+									<Icon icon={link.icon} />
+									{link.name}
+								</a>
+							{/if}
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<div class="inline-flex flex-1 items-center justify-end gap-3 lg:flex-none">
+				<ThemeSwitcher />
+				<a href={resolve('/bookmarks')} class="btn btn-square">
+					<Icon class="h-5 w-5" icon="material-symbols:bookmark" />
+				</a>
+			</div>
+		</nav>
+		<main class="mx-auto mt-2 w-full p-1 lg:w-[80%]">
+			{@render children?.()}
+		</main>
+	</div>
+	<div class="drawer-side">
+		<label for="drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu min-h-full w-80 gap-2 bg-base-200 p-4">
+			<li class="text-lg font-semibold">
+				<a href={resolve('/')} onclick={closeDrawer}>
+					<Icon icon="openmoji:fire-engine" />
+					{PUBLIC_APP_NAME}
+				</a>
+			</li>
+			{#each pageGroups as group (group)}
+				<li class="text-primary">
+					{group.name}
+				</li>
+				{#each group.pages as link (link.href)}
+					<li>
+						{#if page.url.pathname === link.href}
+							<a class="active bg-base-100" href={resolve(link.href)} onclick={closeDrawer}>
+								<Icon icon={link.icon} />
+								{link.name}
+							</a>
+						{:else}
+							<a class="bg-base-100" href={resolve(link.href)} onclick={closeDrawer}>
+								<Icon icon={link.icon} />
+								{link.name}
+							</a>
+						{/if}
+					</li>
+				{/each}
+			{/each}
+		</ul>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,68 +1,75 @@
 <script>
-    import {storageAvailable} from "$lib/helper";
-    import {pageGroups} from "$lib/pages";
-    import {meta} from "$lib/data.json";
-    import Icon from "@iconify/svelte";
-    import { PUBLIC_APP_NAME } from "$env/static/public";
+	import { storageAvailable } from '$lib/helper';
+	import { pageGroups } from '$lib/pages';
+	import { meta } from '$lib/data.json';
+	import Icon from '@iconify/svelte';
+	import { PUBLIC_APP_NAME } from '$env/static/public';
+	import { resolve } from '$app/paths';
 </script>
 
 <svelte:head>
-    <title>{PUBLIC_APP_NAME}</title>
+	<title>{PUBLIC_APP_NAME}</title>
 </svelte:head>
 
 <div>
-    <h1 class="text-3xl text-center font-semibold">
-        {PUBLIC_APP_NAME}
-    </h1>
-    <div class="mt-4 p-4 text-center text-lg bg-base-200 rounded-md">
-        <div class="font-semibold">
-            Quiz zum Vorbereiten auf die hessische Feuerwehrleistungsübung.
-        </div>
-        <div class="divider"></div>
-        Die Fragen stammen aus dem Fragenkatalog der <a class="link link-primary" href="https://hlfs.hessen.de/hessische-feuerwehrleistungsuebung">hessischen Landesfeuerwehrschule</a>.
-        <div class="text-sm">
-            Stand: {meta.date}
-        </div>
-        <div class="divider"></div>
-        <div class="text-sm">
-            Diese Webseite verwendet statische Seiten und benutzt, wenn möglich, den <a class="link link-primary" href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">LocalStorage</a> des
-            <span class="inline-flex items-center gap-2">
-                Browsers.
-                {#if typeof window !== 'undefined' && storageAvailable("localStorage")}
-                <Icon class="text-success" icon="material-symbols:check-circle"/>
-                {:else}
-                <Icon class="text-error" icon="material-symbols:error"/>
-                {/if}
-            </span>
-        </div>
-        <div class="text-sm">
-            Die Konsequenz ist, dass alle Ergebnisse nur im Browser gespeichert werden.
-        </div>
-    </div>
-    {#each pageGroups as group}
-    <div class="flex flex-col gap-2">
-        <div class="divider text-primary text-lg font-bold mb-0">{group.name}</div>
-        <div class="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {#each group.pages as card}
-            <a href={card.href} class="btn h-fit p-2">
-                <div class="flex flex-col justify-center items-center">
-                    <div class="rounded-full text-primary bg-base-100">
-                        <Icon class="w-16 h-16 p-2" icon={card.icon}/>
-                    </div>
-                    <h2 class="text-xl text-primary"> {card.name} </h2>
-                    <p class="wrap-break-word">{card.desc}</p>
-                </div>
-            </a>
-        {/each}
-        </div>
-    </div>
-    {/each}
+	<h1 class="text-center text-3xl font-semibold">
+		{PUBLIC_APP_NAME}
+	</h1>
+	<div class="mt-4 rounded-md bg-base-200 p-4 text-center text-lg">
+		<div class="font-semibold">Quiz zum Vorbereiten auf die hessische Feuerwehrleistungsübung.</div>
+		<div class="divider"></div>
+		Die Fragen stammen aus dem Fragenkatalog der<a
+			class="link link-primary"
+			href="https://hlfs.hessen.de/hessische-feuerwehrleistungsuebung"
+			>hessischen Landesfeuerwehrschule</a
+		>.
+		<div class="text-sm">
+			Stand: {meta.date}
+		</div>
+		<div class="divider"></div>
+		<div class="text-sm">
+			Diese Webseite verwendet statische Seiten und benutzt, wenn möglich, den <a
+				class="link link-primary"
+				href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">LocalStorage</a
+			>
+			des
+			<span class="inline-flex items-center gap-2">
+				Browsers.
+				{#if typeof window !== 'undefined' && storageAvailable('localStorage')}
+					<Icon class="text-success" icon="material-symbols:check-circle" />
+				{:else}
+					<Icon class="text-error" icon="material-symbols:error" />
+				{/if}
+			</span>
+		</div>
+		<div class="text-sm">
+			Die Konsequenz ist, dass alle Ergebnisse nur im Browser gespeichert werden.
+		</div>
+	</div>
+	{#each pageGroups as group (group.name)}
+		<div class="flex flex-col gap-2">
+			<div class="divider mb-0 text-lg font-bold text-primary">{group.name}</div>
+			<div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+				{#each group.pages as card (card.href)}
+					<a href={resolve(card.href)} class="btn h-fit p-2">
+						<div class="flex flex-col items-center justify-center">
+							<div class="rounded-full bg-base-100 text-primary">
+								<Icon class="h-16 w-16 p-2" icon={card.icon} />
+							</div>
+							<h2 class="text-xl text-primary">{card.name}</h2>
+							<p class="wrap-break-word">{card.desc}</p>
+						</div>
+					</a>
+				{/each}
+			</div>
+		</div>
+	{/each}
 </div>
 <footer class="mt-16">
-    <div class="flex justify-center w-full">
-        <a class="btn btn-sm btn-ghost" href="https://github.com/theTCA">
-            <Icon icon="mdi:github"/>
-            theTCA
-        </a>
-    </div>
+	<div class="flex w-full justify-center">
+		<a class="btn btn-ghost btn-sm" href="https://github.com/theTCA">
+			<Icon icon="mdi:github" />
+			theTCA
+		</a>
+	</div>
 </footer>
