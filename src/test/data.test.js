16e3c183ce6b288c83d1a00ef999798deadf76d7
theTCA <t.mattfeld@gmx.de>
2026-01-12 04:06:40 +0100

    Added: new questions & unit testing

---
+++ src/test/data.test.js
@@ -0,0 +1,37 @@
+import { flattendQuestions } from '$lib/data';
+import { expect, test } from 'vitest';
+
+test('Unique ids for questions', () => {
+	const uniqueIds = new Set();
+	/**
+	 * @type {Array<number>}
+	 */
+	let duplicates = [];
+	flattendQuestions.forEach((q) => {
+		if (!uniqueIds.has(q.id)) uniqueIds.add(q.id);
+		else duplicates = [...duplicates, q.id];
+	});
+	expect(duplicates.length, `Duplicate ids @ ${duplicates.toString()}`).toEqual(0);
+});
+
+test('Exaclty one correct answer', () => {
+	/**
+	 * @type {Array<number>}
+	 */
+	let failedAnswers = [];
+	flattendQuestions.forEach((q) => {
+		let choices = [0, 0];
+		q.choices.forEach((c) => {
+			choices[c.answer ? 0 : 1] += 1;
+		});
+		if (choices[0] + choices[1] !== 3) {
+			failedAnswers = [...failedAnswers, q.id];
+			return;
+		}
+		if (choices[0] !== 1) {
+			failedAnswers = [...failedAnswers, q.id];
+			return;
+		}
+	});
+	expect(failedAnswers.length, `Answers wrong @ ${failedAnswers.toString()}`).toEqual(0);
+});
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
+import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	test: {
+		expect: { requireAssertions: true },
+
+		projects: [
+			{
+				extends: './vite.config.js',
+
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
 });
