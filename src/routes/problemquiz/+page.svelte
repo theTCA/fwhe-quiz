e8c48fe5819fa0114704a74a11de54a97cf46e98
theTCA <t.mattfeld@gmx.de>
2026-01-17 16:26:49 +0100

    Updated: problem quiz to svelte5

--- src/routes/problemquiz/+page.svelte
+++ src/routes/problemquiz/+page.svelte
@@ -1,69 +1,75 @@
 <script>
-    import { PUBLIC_APP_NAME } from "$env/static/public";
-    import { shuffle } from "$lib/helper";
-    import { quizHistory } from "$lib/stores";
-    import Question from "$lib/components/Question.svelte";
-    import QuestionHelp from "$lib/components/QuestionHelp.svelte";
-    import QuestionDisplay from "$lib/components/QuestionDisplay.svelte";
-    import { base } from "$app/paths";
+	import { PUBLIC_APP_NAME } from '$env/static/public';
+	import { shuffle } from '$lib/helper';
+	import { quizHistory } from '$lib/stores';
+	import Question from '$lib/components/Question.svelte';
+	import QuestionHelp from '$lib/components/QuestionHelp.svelte';
+	import QuestionDisplay from '$lib/components/QuestionDisplay.svelte';
+	import { resolve } from '$app/paths';
 
-    let questions = getPossibleQuestions();
-    let currentIndex = 0;
-    let answer = "";
-    let answered = false;
+	let questions = getPossibleQuestions();
+	let currentIndex = $state(0);
+	let answer = $state('');
+	let answered = $state(false);
 
-    function getPossibleQuestions() {
-        let possibleQuestions = $quizHistory.filter((/** @type {import("$lib/types").QuizHistory} */ h) => h.answer !== h.question.choices.find(c => c.answer)?.text).map((/** @type {import("$lib/types").QuizHistory} */ h) => h.question);
-        possibleQuestions = shuffle(possibleQuestions.map((/** @type {import("$lib/types").Question} */ q) => ({...q, choices: shuffle(q.choices)})));
-        return possibleQuestions;
-    }
+	function getPossibleQuestions() {
+		/** @type {import('$lib/types').QuizHistory[]}*/
+		let possibleQuestions = [];
+		if ('quiz' in $quizHistory) possibleQuestions = [...$quizHistory['quiz']];
+		if ('endless' in $quizHistory)
+			possibleQuestions = [...possibleQuestions, ...$quizHistory['endless']];
+		let questions = possibleQuestions
+			.filter((h) => h.answer !== h.question.choices.find((c) => c.answer)?.text)
+			.map((h) => h.question);
+		questions = shuffle(
+			questions.map((q) => ({
+				...q,
+				choices: shuffle(q.choices)
+			}))
+		);
+		return questions;
+	}
 
-    function nextQuestion() {
-        if(currentIndex + 1 > questions.length)
-            currentIndex = 0;
-        else
-            currentIndex++;
-        answer = ""
-        answered = false;
-    }
+	function nextQuestion() {
+		if (currentIndex + 1 > questions.length) currentIndex = 0;
+		else currentIndex++;
+		answer = '';
+		answered = false;
+	}
 
-    function answerQuestion() {
-        answered = true;
-    }
+	function answerQuestion() {
+		answered = true;
+	}
 </script>
 
 <svelte:head>
-    <title> Problemquiz - { PUBLIC_APP_NAME } </title>
+	<title>Problemquiz - {PUBLIC_APP_NAME}</title>
 </svelte:head>
 
 <div>
-    <h1 class="text-3xl text-center font-semibold mb-2"> Problemquiz </h1>
-    {#if questions.length > 0}
-    <div class="mb-2">
-        {#if !answered}
-        <Question question={questions[currentIndex]} bind:answer/>
-        {:else}
-        <QuestionDisplay question={questions[currentIndex]} {answer}/>
-        {/if}
-    </div>
-    <div class="grid grid-cols-2 gap-2 mb-4">
-        <a class="btn btn-outline" href={base + "/history"}> Zurück </a>
-        {#if answered}
-        <button class="btn btn-primary" on:click={nextQuestion}> Weiter </button>
-        {:else}
-        {#if answer.length > 0}
-        <button class="btn btn-primary" on:click={answerQuestion}> Wählen </button>
-        {:else}
-        <button class="btn btn-primary btn-outline" on:click={answerQuestion}> Wählen </button>
-        {/if}
-        {/if}
-    </div>
-    {#if answered}
-    <QuestionHelp question={questions[currentIndex]}/>
-    {/if}
-    {:else}
-    <div class="text-center">
-        Keine Problemfragen!
-    </div>
-    {/if}
+	<h1 class="mb-2 text-center text-3xl font-semibold">Problemquiz</h1>
+	{#if questions.length > 0}
+		<div class="mb-2">
+			{#if !answered}
+				<Question question={questions[currentIndex]} bind:answer />
+			{:else}
+				<QuestionDisplay question={questions[currentIndex]} {answer} showCatalog />
+			{/if}
+		</div>
+		<div class="mb-4 grid grid-cols-2 gap-2">
+			<a class="btn btn-soft" href={resolve('/')}> Zurück </a>
+			{#if answered}
+				<button class="btn btn-primary" onclick={nextQuestion}> Weiter </button>
+			{:else if answer.length > 0}
+				<button class="btn btn-primary" onclick={answerQuestion}> Wählen </button>
+			{:else}
+				<button class="btn btn-outline btn-primary" onclick={answerQuestion}> Wählen </button>
+			{/if}
+		</div>
+		{#if answered}
+			<QuestionHelp question={questions[currentIndex]} />
+		{/if}
+	{:else}
+		<div class="text-center">Keine Problemfragen!</div>
+	{/if}
 </div>
