bcd5a24077f36d6382de77f187f07ce3e89793df
theTCA <t.mattfeld@gmx.de>
2026-02-09 01:15:55 +0100

    Fixed: image overflow

--- src/lib/components/QuestionHeader.svelte
+++ src/lib/components/QuestionHeader.svelte
@@ -13,7 +13,7 @@
 		<span class="text-xs font-bold opacity-65 lg:text-sm">aus {question.catalog}</span>
 	{/if}
 	{#if question.image}
-		<div class="mx-auto h-32 w-32 py-1">
+		<div class="mx-auto w-32 py-1">
 			<img
 				src={asset(`/img/${images.find((i) => i.id === question.image)?.file}`)}
 				alt={images.find((i) => i.id === question.image)?.description}
